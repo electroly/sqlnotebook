49c2e9212a8c336a2b643a05b111a9f14cfd3d46
Brian Luft <brian@electroly.com>
2016-04-17 00:31:32 -0400

    Version 0.3

--- src/SqlNotebook/Properties/AssemblyInfo.cs
+++ src/SqlNotebook/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // this is Application.ProductVersion
-[assembly: AssemblyFileVersion("0.2")]
+[assembly: AssemblyFileVersion("0.3")]
