8378d76c75d94a2b8560a4e4c3c24d0d11a20efa
Brian Luft <brian@electroly.com>
2016-07-24 21:19:07 -0400

    Fix AssemblyInfo.cs

--- src/SqlNotebookScript/Properties/AssemblyInfo.cs
+++ src/SqlNotebookScript/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SqlNotebookCoreModules")]
+[assembly: AssemblyTitle("SqlNotebookScript")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlNotebookCoreModules")]
+[assembly: AssemblyProduct("SqlNotebookScript")]
 [assembly: AssemblyCopyright("Copyright © 2016 Brian Luft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
