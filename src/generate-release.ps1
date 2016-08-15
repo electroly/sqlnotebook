39d1ac2c0662eb78e19aa2a70fb20f896d1c6bf7
Brian Luft <brian@electroly.com>
2016-08-15 19:49:00 -0400

    Use WiX instead of ClickOnce.

--- src/generate-release.ps1
+++ src/generate-release.ps1
@@ -1,53 +1,111 @@
-﻿# Release procedure:
+$ErrorActionPreference = "Stop"
+
+# Release procedure:
 # - Switch to Release build configuration
-# - Delete "echo" in post-build event for running signtool
 # - Delete src\SqlNotebook\Resources\sqlite-doc.zip
-# - Delete publish\
 # - Bump AssemblyFileVersion in src\SqlNotebook\Properties\AssemblyInfo.cs
-# - Bump publish version in project properties under Publish
+# - Bump ProductVersion in SqlNotebook.wxs
+# - Delete all files in Release\
 # - Rebuild
-# - Verify version, signature, and signature timestamp in Release\SqlNotebook.exe
-# - Publish
 # - Run src\generate-release.ps1
-# - Unzip publish\SqlNotebook_X_X_X_X.zip to test
-# - Update web\download.md with new version and date, also update portable zip link
+# - Verify version, signature, and timestamp in Release\SqlNotebook.exe and .msi
+# - Update web\download.md with new version and date, also update two download links
 # - Delete web\site\
 # - Run web\generate-site.ps1
 # - Open S3 Management Console in sqlnotebook.com bucket
-# - Upload publish\SqlNotebook_X_X_X_X.zip to sqlnotebook.com/install. Make public.
-# - Upload publish\Application Files\SqlNotebook_X_X_X_X. Make public.
-# - Upload publish\SqlNotebook.application and setup.exe. Make public.
+# - Upload Release\SqlNotebook_X_X_X_X.zip and .msi to sqlnotebook.com/install.  Make public.
 # - Upload web\site. Make public.
-# - Replace "echo" in post-build event for running signtool
 # - Commit changes using commit message "Version X.X"
 # - Push origin master
 # - Make release on GitHub
 
-# Before running this script, publish the ClickOnce installer in Visual Studio.
-# ..\publish\ will be populated.
+# ---
 
-# Re-sign setup.exe so it has a timestamp (VS already signed it, but without a timestamp)
-& "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /n "Brian Luft" /tr http://tsa.startssl.com/rfc3161 ..\publish\setup.exe
+$version = Read-Host -prompt "Version (like 0_6_0)"
 
-Remove-Item ..\publish-portable -Recurse -ErrorAction SilentlyContinue
-mkdir ..\publish-portable
+$srcdir = [System.IO.Path]::GetDirectoryName($PSCommandPath)
+$reldir = (Resolve-Path "$srcdir\..\Release")
+$wixdir = "C:\Program Files (x86)\WiX Toolset v3.10\bin"
 
-# folder: 'SqlNotebook_0_4_0_0'
-$folder = (dir '..\publish\Application Files' | ForEach {$_.Name} | Sort {$_} | Select -Last 1)
-# absFolder: 'C:\Projects\sqlnotebook\publish\Application Files\SqlNotebook_0_4_0_0'
-$absFolder = (Resolve-Path ('..\publish\Application Files\' + $folder))
+Push-Location $reldir
 
-copy "$absFolder\*.*" ..\publish-portable\
-dir ..\publish-portable\*.deploy | ForEach {Rename-Item $_.FullName $_.FullName.Replace(".deploy", "")}
-del ..\publish-portable\SqlNotebook.application
+#
+# Part 1: Generate MSI
+#
 
-copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\*.dll" ..\publish-portable\
+$msiFilename = "SQLNotebook_$version.msi"
+$zipFilePath = "$reldir\SQLNotebook_$version.zip"
+
+rm "$reldir\$msiFilename" -ErrorAction SilentlyContinue
+rm $zipFilePath -ErrorAction SilentlyContinue
+rm "$reldir\SqlNotebook.wixobj" -ErrorAction SilentlyContinue
+rm "$reldir\SqlNotebook.wxs" -ErrorAction SilentlyContinue
+
+# Replace <!--UCRT_FILES--> in the wxs file with <File> entries for the C runtime
+$crtFilesXml = ""
+$crtFileId = 1
+
+$ucrtDllPath = "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64"
+$ucrtFilenames = ls "$ucrtDllPath\*.dll" | select -expandprop Name
+foreach ($filename in $ucrtFilenames) {
+    $crtFilesXml += "<File Id=""_Crt$crtFileId"" Source=""$ucrtDllPath\$filename""/>"
+    $crtFileId++
+}
+
+$vcrDllPath = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT"
+$vcrFilesXml = ""
+$vcrFilenames = ls "$vcrDllPath\*.dll" | select -expandprop Name
+foreach ($filename in $vcrFilenames) {
+    $crtFilesXml += "<File Id=""_Crt$crtFileId"" Source=""$vcrDllPath\$filename""/>"
+    $crtFileId++
+}
+
+$wxs = (Get-Content "$srcdir\SqlNotebook.wxs").Replace("<!--UCRT_FILES-->", $crtFilesXml)
+Set-Content "$reldir\SqlNotebook.wxs" $wxs
+
+copy -force "$srcdir\SqlNotebook\SqlNotebookIcon.ico" "$reldir\SqlNotebookIcon.ico"
+
+& "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /n "Brian Luft" /tr http://tsa.startssl.com/rfc3161 "$reldir\SqlNotebook.exe"
+
+& "$wixdir\candle.exe" -nologo -pedantic "$reldir\SqlNotebook.wxs"
+
+if (-not (Test-Path "$reldir\SqlNotebook.wixobj")) {
+    throw "candle failed to produce SqlNotebook.wixobj"
+}
+
+& "$wixdir\light.exe" -nologo -pedantic "$reldir\SqlNotebook.wixobj"
+
+if (-not (Test-Path "$reldir\SqlNotebook.msi")) {
+    throw "light failed to produce SqlNotebook.msi"
+}
+
+$msiFilePath = "$reldir\$msiFilename"
+mv "$reldir\SqlNotebook.msi" $msiFilePath
+
+& "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /n "Brian Luft" /tr http://tsa.startssl.com/rfc3161 /d $msiFilename $msiFilePath
+
+#
+# Part 2: Generate ZIP
+#
+
+Remove-Item "$reldir\portable" -Recurse -ErrorAction SilentlyContinue
+mkdir "$reldir\portable" -ErrorAction SilentlyContinue
+
+# get the list of necessary files from the .wxs file
+$fileLines = [System.IO.File]::ReadAllLines("$srcdir\SqlNotebook.wxs")
+foreach ($line in $fileLines) {
+    if ($line.Contains("<File")) {
+        $line = $line.Replace('<File Id="', "").Trim()
+        $filename = $line.Substring(0, $line.IndexOf('"'))
+        copy "$reldir\$filename" "$reldir\portable\$filename"
+    }
+}
+
+copy "$ucrtDllPath\*.dll" "$reldir\portable\"
+copy "$vcrDllPath\*.dll" "$reldir\portable\"
 
 Add-Type -AssemblyName System.IO.Compression
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-$inFolder = (Resolve-Path ..\publish-portable)
-$outZip = [System.IO.Path]::Combine((Resolve-Path ..\publish), $folder + ".zip")
-Remove-Item "..\publish\$folder.zip" -ErrorAction SilentlyContinue
-[System.IO.Compression.ZipFile]::CreateFromDirectory($inFolder, $outZip)
+[System.IO.Compression.ZipFile]::CreateFromDirectory("$reldir\portable", $zipFilePath)
 
-Remove-Item ..\publish-portable -Recurse -ErrorAction SilentlyContinue
+Pop-Location
--- web/download.md
+++ web/download.md
@@ -1,28 +1,14 @@
 # Download & Install
 
-The current version is **0.6** and was released on **August 14, 2016**.  SQL Notebook requires a 64-bit computer running Windows 7 or higher.  This software is open source ([MIT license](license.html)) and available free of charge.  [View the release history on GitHub](https://github.com/electroly/sqlnotebook/releases).
+The current version is **0.6.0** and was released on **August 14, 2016**.  ([Release history](https://github.com/electroly/sqlnotebook/releases))
+
+This software is open source ([MIT license](license.html)) and available free of charge.
 <br><br>
 
-### [Download Installer .EXE (preferred)](https://sqlnotebook.com/install/setup.exe)
-
-This is a [ClickOnce](https://en.wikipedia.org/wiki/ClickOnce) installer.  It will install all necessary prerequisites, and SQL Notebook will automatically check for updates.
+<h1><a href="install/SQLNotebook_0_6_0.msi" style="background: #2ca089; color: white; padding: 10px; padding-left: 20px; padding-right: 20px; border-radius: 6px;">Download SQL Notebook</a></h1>
+Required: 64-bit system running Windows 7 (or newer) with [.NET Framework 4.6](https://www.microsoft.com/en-us/download/details.aspx?id=53345).
 <br><br>
 
-### [Download Portable .ZIP](https://sqlnotebook.com/install/SqlNotebook_0_6_0_0.zip)
-
-This portable app does not require installation.  You must already have [.NET Framework 4.6](https://www.microsoft.com/en-us/download/details.aspx?id=53345) installed.  This version of SQL Notebook will *not* check for updates.  Come back to this page to download new versions.
+**[Download Portable .ZIP](install/SQLNotebook_0_6_0.zip)**    
+This portable app does not require installation.  Simply run `SqlNotebook.exe`.
 <br><br>
-
-### Instructions
-
-Because SQL Notebook is a fresh new application that Microsoft has not seen before, the installation may trigger a Windows SmartScreen prompt.  These prompts will eventually go away as Microsoft registers more downloads for the application.  Until then, the prompt can be bypassed:
-
-<img src="art/smartscreen.gif" style="float: right; margin-left: 30px; margin-top: -10px;">
-
-1. Click the "More Info" link.
-
-2. Verify that the publisher is "Brian Luft."
-
-3. Click the "Run anyway" button.
-
-Always read security prompts carefully.
