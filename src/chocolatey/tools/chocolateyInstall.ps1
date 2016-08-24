6ba0d49feab8d256600975f4f62893bbb8874ea6
Brian Luft <brian@electroly.com>
2016-08-23 23:22:02 -0400

    Add checksum to chocolatey package.

--- src/chocolatey/tools/chocolateyInstall.ps1
+++ src/chocolatey/tools/chocolateyInstall.ps1
@@ -5,5 +5,6 @@ $installerType = 'msi'
 $url = 'https://sqlnotebook.com/install/SQLNotebook_0_6_0.msi'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
+$checksum = '336E6C78C2E7A01C9A983FAE96FFA032AB2864CAC8FC58809205F587F0E40369'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksumType 'sha256' -checksum $checksum
