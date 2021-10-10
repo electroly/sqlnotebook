3f4b3f743b179e4831ad7e6e9fd965e7676f6586
Brian Luft <brian@electroly.com>
2021-10-10 18:57:27 -0400

    Auto-format HTML doc files

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,11 +1,30 @@
-# Guide for contributors
+# Contributors Guide
 
-## How to build from source
+A Windows build machine is assumed.
 
-- A Windows machine is required.
-- Install Visual Studio 2019 with:
+- Install Visual Studio 2019.
     - ".NET desktop development" workload
     - "Desktop development with C++" workload
     - "C++/CLI support for v142 build tools (Latest)" component
+- Install WSL with Ubuntu 20.04.
+    - Install some tools: `sudo apt-get install tidy`
+    - Install PowerShell:
+        ```
+        sudo apt-get update
+        sudo apt-get install -y wget apt-transport-https software-properties-common
+        wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+        sudo dpkg -i packages-microsoft-prod.deb
+        sudo apt-get update
+        sudo apt-get install -y powershell
+        ```
+- Install SeaMonkey from https://www.seamonkey-project.org/releases/
+
+## How to build from source
+
 - Run `src\SqlNotebook\Resources\generate-sqlite-doc-zip.ps1` to generate `sqlite-doc.zip`.
 - Open `src\SqlNotebook.sln` and build.
+
+## How to edit HTML files
+
+- Use SeaMonkey Composer to edit.
+- In WSL, run `ps1/Update-HtmlFormatting.ps1` to fix up the HTML formatting after saving in SeaMonkey.
---
+++ ps1/Update-HtmlFormatting.ps1
@@ -0,0 +1,32 @@
+#!/usr/bin/env pwsh
+# Formats the HTML files under doc/. Must have HTML Tidy installed (see CONTRIBUTING.md).
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 3
+
+$ps1Dir = $PSScriptRoot
+$docDir = Resolve-Path (Join-Path $ps1Dir "..\doc")
+
+$htmlFilenames = Get-Item (Join-Path $docDir "*.html") | % { $_.Name }
+foreach ($htmlFilename in $htmlFilenames) {
+    echo $htmlFilename
+    $htmlFilePath = Join-Path $docDir $htmlFilename
+    $tempFilePath = "$htmlFilePath.tmp"
+    Remove-Item -Force $tempFilePath -ErrorAction SilentlyContinue
+
+    # Write reformatted HTML to $tempFilePath
+    tidy -q -utf8 -i -w 120 -o "$tempFilePath" "$htmlFilePath"
+    if (-not (Test-Path $tempFilePath)) {
+        throw "Tidy failed on `"$htmlFilePath`"!"
+    }
+    $html = [System.IO.File]::ReadAllText($tempFilePath)
+
+    # Remove the <meta name="generator" ...> tag that HTML Tidy inserts.
+    $indexStart = $html.IndexOf('<meta name="generator"')
+    $indexStart = $html.LastIndexOf("`n", $indexStart)
+    $indexEnd = $html.IndexOf('>', $indexStart)
+    $html = $html.Substring(0, $indexStart) + $html.Substring($indexEnd + 1)
+
+    # Write back to $htmlFilePath
+    [System.IO.File]::WriteAllText($htmlFilePath, $html)
+    Remove-Item -Force $tempFilePath
+}
