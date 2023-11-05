2e65f3240f5740f9cd5efcca843b78d9a64b5d03
Brian Luft <brian@electroly.com>
2023-11-05 14:38:06 -0400

    Update website and Chocolatey to version 1.2.2

--- src/chocolatey/tools/chocolateyInstall.ps1
+++ src/chocolatey/tools/chocolateyInstall.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = "Stop"
 
 $packageName = 'SQL Notebook'
 $installerType = 'msi'
-$url = 'https://github.com/electroly/sqlnotebook/releases/download/v1.2.1/SQLNotebook-1.2.1.msi'
+$url = 'https://github.com/electroly/sqlnotebook/releases/download/v1.2.2/SqlNotebook-64bit-1.2.2.msi'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
-$checksum = 'DEF5C511AE68EF79DC62138D09632BB834448355DD46E25CAB7B1228916EA792'
+$checksum = 'F13F50F24637E0CAD2A3531B0A57DF0617BEC983B20208B1B056933D89D5AF0C'
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksumType 'sha256' -checksum $checksum
